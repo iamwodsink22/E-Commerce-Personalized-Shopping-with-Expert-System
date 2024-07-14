808b9cbd7326cd4103b978b4af655a6c77e3fc14
unknown <arakshapuri22@gmail.com>
2024-07-14 13:50:17 +0545

    last

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,61 +1,13 @@
-import numpy as np
-import copy
+import axios from "axios";
 
+const axiosInstance = axios.create({baseURL: 'https://e-commerce-personalized-shopping-with-expert-system-t9j2.vercel.app/api'});
 
-class Env:
-    def __init__(self, states, player):
-        self.states = states
-        self.player = player
-   
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) =>
+    Promise.reject(
+      (error.response && error.response.data) || "Something went wrong"
+    )
+);
 
-    def get_moves(self):
-        return list(zip(*np.where(self.states == 0)))
-
-    def get_next_player(self):
-        self.player = self.player*-1
-    
-
-    def next_state(self, action, player):
-        prime_state = copy.deepcopy(self.states)
-        print(action)
-        prime_state[action] = player
-
-        return (prime_state, -player)
-
-    
-
-    def get_cboard(self, state, player):
-        return player*state
-
-   
-    def winner(self,  player):
-        seqs = np.array([self.states[0, :], self.states[1, :], self.states[2, :],
-                        self.states[:, 0], self.states[:, 1],
-                        self.states[:, 2], self.states[(0, 1, 2), (0, 1, 2)], self.states[(2, 1, 0), (0, 1, 2)]], dtype='object')
-        seq = seqs.tolist()
-
-        if [player, player, player] in seq:
-            return True
-        else:
-            return False
-   
-
-    def get_reward(self, player):
-        if self.winner(player):
-            return 10
-        if self.winner(-player):
-            return -10
-        else:
-            return 0
-
-    def game_end(self):  
-        if 0 not in self.states or self.game_over():
-            return True
-        else:
-            return False
-
-    def game_over(self): 
-        if self.winner(1) or self.winner(-1) is True:
-            return True
-        else:
-            return False
+export default axiosInstance;
